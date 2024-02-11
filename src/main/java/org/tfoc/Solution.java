d3176dc0b10bd06130fbe44ad914fb3365ba2a4b
Alejandro Portilla Gainza <aportiga@mercadona.es>
2024-02-11 22:01:55 +0100

    feat: Valid chain

--- src/main/java/org/tfoc/Solution.java
+++ src/main/java/org/tfoc/Solution.java
@@ -1,13 +1,63 @@
 package org.tfoc;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 /**
  * The class containing the solution to this exercise
  */
 public class Solution {
 
-    public boolean isValid(String s) {
+    public static boolean isValid(String s) {
+        Map<String, String> mapPatternCaracteres = Map.of(
+                "(", ")",
+                "{", "}",
+                "[", "]"
+        );
 
-        return false;
+        Map<Integer, Boolean> mapCaracterUsed = new HashMap<>();
+
+        char[] chainCaracteres = s.toCharArray();
+        int lengthChain = chainCaracteres.length;
+
+        for (int i = 0; i < chainCaracteres.length; i++) {
+            mapCaracterUsed.put(i, false);
+        }
+
+        for (Map.Entry<Integer, Boolean> entry : mapCaracterUsed.entrySet()) {
+            Integer key = entry.getKey();
+            Boolean value = entry.getValue();
+            String chainCaractere = "" + chainCaracteres[key];
+
+            if (!mapPatternCaracteres.containsKey(chainCaractere) &&
+                    !mapPatternCaracteres.containsValue(chainCaractere)) {
+                break;
+            }
+
+            String keyCaracterClose = mapPatternCaracteres.get(chainCaractere);
+
+            if (!value) {
+                boolean fincCaracterClose = false;
+                for (int i = key + 1; i < lengthChain; i++) {
+                    if (Objects.nonNull(keyCaracterClose)) {
+                        String chainCaractere1 = "" + chainCaracteres[i];
+                        if (keyCaracterClose.equals(chainCaractere1)) {
+                            mapCaracterUsed.put(i, true);
+                            fincCaracterClose = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (fincCaracterClose)
+                    entry.setValue(true);
+
+            }
+
+        }
+
+        return mapCaracterUsed.values().stream().allMatch(Boolean::booleanValue);
     }
 
 }
--- src/test/java/org/tfoc/SolutionTest.java
+++ src/test/java/org/tfoc/SolutionTest.java
@@ -1,15 +1,36 @@
 package org.tfoc;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Slf4j
 class SolutionTest {
 
-    @Test
-    void testSolution() {
-
-        log.info("Don't forget to write the tests to make sure it works. Enjoy!");
+    private static Stream<Arguments> provideInputString() {
+        return Stream.of(
+                Arguments.of("()", true),
+                Arguments.of("()[]{}", true),
+                Arguments.of("(]", false),
+                Arguments.of("{[]}", true),
+                Arguments.of("({[]})", true),
+                Arguments.of("{]}", false),
+                Arguments.of("({[]})()({[]})", true)
+        );
     }
 
+    @ParameterizedTest
+    @MethodSource("provideInputString" )
+    void testSolution(String chain, Boolean responseExpected) {
+
+        boolean response = Solution.isValid(chain);
+
+        assertEquals(responseExpected, response);
+
+    }
 }
